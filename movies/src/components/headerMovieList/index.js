d43fd8dc4012a8a7682f0a78c69e9723ab12cd76
Frank Walsh <fxwalsh@wit.ie>
2023-10-20 15:56:14 +0100

    arrow

--- movies/src/components/headerMovieList/index.js
+++ movies/src/components/headerMovieList/index.js
@@ -4,9 +4,11 @@ import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
 
 const Header = (props ) => {
   const title = props.title
+  const navigate = useNavigate();
   return (
     <Paper 
       component="div" 
@@ -20,7 +22,9 @@ const Header = (props ) => {
       <IconButton
         aria-label="go back"
       >
+             <IconButton aria-label="go back" onClick={() => navigate(-1)}>
         <ArrowBackIcon color="primary" fontSize="large" />
+      </IconButton>
       </IconButton>
 
       <Typography variant="h4" component="h3">
@@ -29,7 +33,9 @@ const Header = (props ) => {
       <IconButton
         aria-label="go forward"
       >
+              <IconButton aria-label="go forward" onClick={() => navigate(+1)}>
         <ArrowForwardIcon color="primary" fontSize="large" />
+      </IconButton>
       </IconButton>
     </Paper>
   );
