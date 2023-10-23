28ca300030f3c4f751ddd9de0ee4ebc3aef01ab1
Frank Walsh <fxwalsh@wit.ie>
2023-10-23 16:37:47 +0100

    Fix upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage';
 import { getUpcomingMovies } from "../api/tmdb-api";
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites';
 
 const HomePage = (props) => {
   const [movies, setMovies] = useState([]);
@@ -24,7 +25,9 @@ const HomePage = (props) => {
     <PageTemplate
       title='Upcoming Movies'
       movies={movies}
-      selectFavorite={addToFavorites}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}
     />
   );
 };
